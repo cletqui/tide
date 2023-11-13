376dc084601ccd17b4127d3d44c7def2ea74dd30
cletqui <antoine.quillivic@gmail.com>
2023-11-13 21:07:35 +0100

    v1.0.3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 /* Global Constants */
 
-const TIDE_DATA_URL = "https://api.cybai.re/tide?id=72";
-const INTERVAL = 1000;
+const TIDE_DATA_URL = "https://api.cybai.re/tide?name=";
+const HARBOURS_DATA_URL = "https://api.cybai.re/tide/harbours?name=";
+const INTERVAL = 60000;
 
 /* Global Variables */
 
 let isDarkMode = true;
+let harbourName = "";
 let tide = {};
 
 /* Display Functions */
@@ -46,31 +48,29 @@ const initiateTheme = () => {
   htmlElement.dataset.theme = themeAttribute;
 };
 
-function updateHand(degrees) {
-  const hand = document.querySelector(".hand");
-  hand.style.transform = `rotate(${degrees}deg)`;
-}
-
-/* Display Functions */
-
-const fetchTide = async () => {
-  try {
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(TIDE_DATA_URL, requestOptions);
-    console.log(response);
-    const tideData = await response.json(); // Parse the response as JSON
-    tide = tideData;
-    return tideData;
-  } catch (error) {
-    console.error(`Error fetching tide data: ${error}`);
-  }
+const refreshSearchBar = (harbour = harbourName) => {
+  const searchBar = document.getElementById("search-bar");
+  window.localStorage.setItem("harbourName", harbour);
+  searchBar.placeholder = harbour;
 };
 
+/**
+ * Updates the position of a hand element on the page by rotating it to the specified degrees.
+ *
+ * @param {number} degrees - The degrees to rotate the hand element.
+ * @returns {void}
+ */
+const updateHand = (degrees) => {
+  const hand = document.querySelector(".hand");
+  hand.style.transform = `rotate(${degrees}deg)`;
+};
+
+/**
+ * Sets the time and updates the hand position based on the current tide information.
+ *
+ * @function setTime
+ * @returns {void}
+ */
 const setTime = () => {
   const { last_tide, next_tide } = tide;
   let degrees = 0;
@@ -78,31 +78,27 @@ const setTime = () => {
   const now_timestamp = new Date().toLocaleString("fr-FR", {
     timeZone: "Europe/Paris",
   });
-  const now = new Date(now_timestamp);
+  const now = createDateObject(now_timestamp);
 
   if (last_tide) {
     const [_, { timestamp: last_timestamp }] = Object.entries(last_tide)[0];
     const [next_tide_type, { timestamp: next_timestamp }] =
       Object.entries(next_tide)[0];
-    const last_date = new Date(last_timestamp);
-    const next_date = new Date(next_timestamp);
+    const last_date = createDateObject(last_timestamp);
+    const next_date = createDateObject(next_timestamp);
 
-    if ((last_timestamp <= now_timestamp) & (now_timestamp <= next_timestamp)) {
-      console.log("caca");
+    if (last_timestamp <= now_timestamp && now_timestamp <= next_timestamp) {
       degrees = calculateDegrees(next_tide_type, now, last_date, next_date);
     } else {
       degrees = next_tide_type == "high_tide" ? 90 : 270; // Keep hand in specific place regarding the last known tide type
-      fetchTide(); // Start a fetch to update tide data
-      console.error(
-        `Last and next tides unsynchronised with now time:\nlast_tide = ${last_timestamp}\nnow = ${now_timestamp}\nnext_tide = ${next_timestamp}`
-      );
+      fetchTide().then((tideData) => (tide = tideData)); // Update tide data to fetch new next tide
     }
   } else if (next_tide) {
     // If last tide was the day before, last_tide will be null. We extrapolate last tide from next tide
     const [next_tide_type, { timestamp: next_timestamp }] =
       Object.entries(next_tide)[0];
-    const next_date = new Date(next_timestamp);
-    const last_date = new Date(next_timestamp);
+    const next_date = createDateObject(next_timestamp);
+    const last_date = createDateObject(next_timestamp);
     last_date.setHours(last_date.getHours() - 6); // Construct a fictional last tide date 6 hours before the next date
 
     degrees = calculateDegrees(next_tide_type, now, last_date, next_date);
@@ -111,15 +107,115 @@ const setTime = () => {
   updateHand(degrees);
 };
 
-function calculateDegrees(next_tide_type, now, last_date, next_date) {
+/* Operating Functions */
+
+/**
+ * Fetches tide data from the specified URL and returns it as JSON.
+ *
+ * @async
+ * @function fetchTide
+ * @returns {Promise<Object>} A promise that resolves to the fetched tide data as JSON.
+ * @throws {Error} If there is an error fetching the tide data.
+ */
+const fetchTide = async (name) => {
+  // const response = await fetch("../tide.json");
+  // return await response.json();
+  // TODO sanitize name input
+  const url = `${TIDE_DATA_URL}Roscoff`;
+  console.log(url);
+  try {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(TIDE_DATA_URL, requestOptions);
+    return await response.json(); // Parse the response as JSON
+  } catch (error) {
+    console.error(`Error fetching tide data: ${error}`);
+  }
+};
+
+const fetchHarbours = async (name) => {
+  // TODO sanitize name
+  const url = `${HARBOURS_DATA_URL}${name}`;
+  try {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(url, requestOptions);
+    return await response.json(); // Parse the response as JSON
+  } catch (error) {
+    console.error(`Error fetching tide data: ${error}`);
+  }
+};
+
+/**
+ * Creates a Date object from a timestamp string in "dd/mm/yyyy hh:mm:ss" format.
+ *
+ * @param {string} timestamp - The timestamp string to create the Date object from.
+ * @returns {Date|undefined} The created Date object, or undefined if the timestamp is invalid.
+ */
+const createDateObject = (timestamp) => {
+  const [date, time] = timestamp.split(" ");
+  if (date && time) {
+    [day, month, year] = date.split("/");
+    [hours, minutes, seconds] = time.split(":");
+    return new Date(year, month, day, hours, minutes, seconds);
+  }
+};
+
+/**
+ * Calculates the degrees for the next tide based on the current time and previous and next tide dates.
+ *
+ * @param {string} next_tide_type - The type of the next tide ("low_tide" or "high_tide").
+ * @param {number} now - The current time in milliseconds since January 1, 1970.
+ * @param {number} last_date - The date of the previous tide in milliseconds since January 1, 1970.
+ * @param {number} next_date - The date of the next tide in milliseconds since January 1, 1970.
+ * @returns {number} The degrees for the next tide.
+ */
+const calculateDegrees = (next_tide_type, now, last_date, next_date) => {
   const start = next_tide_type == "low_tide" ? 90 : 270;
   const offset = (180 * (now - last_date)) / (next_date - last_date);
   return start + offset;
-}
+};
+
+/**
+ * Handles the input event of a search bar.
+ *
+ * @param {KeyboardEvent} event - The input event object.
+ * @returns {void}
+ */
+const searchBarInput = async (event) => {
+  if (event.key === "Enter") {
+    const harbour = event.target.value;
+    const nearestHarbours = await fetchHarbours(harbour);
+    console.log(nearestHarbours);
+    refreshSearchBar(harbour);
+    event.target.value = "";
+  }
+};
+
+/* Startup Function */
+
+/**
+ * Adds event listeners to various buttons in the application.
+ */
+const addEventListeners = () => {
+  const searchBar = document.getElementById("search-bar");
+  searchBar.addEventListener("keypress", (e) => searchBarInput(e));
+};
 
 document.addEventListener("DOMContentLoaded", async function () {
+  // TODO initiate the clock with the nearest harbour
+  addEventListeners();
   initiateTheme();
-  await fetchTide();
+  refreshSearchBar();
+  tide = await fetchTide();
   setTime();
-  setInterval(setTime, 60000);
+  setInterval(setTime, INTERVAL);
 });
